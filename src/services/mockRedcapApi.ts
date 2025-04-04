7f067afd84ebc1f60b4a4a56324cef3c5057a765
stormliucong <stormliucong@gmail.com>
2025-04-04 12:52:10 -0400

    update mock redcap participant

--- src/services/mockRedcapApi.ts
+++ src/services/mockRedcapApi.ts
@@ -24,10 +24,19 @@ export class MockRedcapApiService {
         record_id: `NHS${i + 1}`,
         age: this.getRandomAgeGroup(),
         consent_location: Math.random() > 0.5 ? 'Boston' : 'Others',
-        location_country: Math.random() > 0.3 ? 'United States' : 'Others', // 70% chance of being from US
+        location_country: Math.random() > 0.3 ? 'United States' : 'Others',
         gene: geneName,
         consent_date: this.getRandomDate(new Date(2023, 0, 1), new Date()),
-        survey_completion_date: this.getRandomDate(new Date(2023, 0, 1), new Date())
+        race: this.getRandomRace(),
+        ethnicity: this.getRandomEthnicity(),
+        mhi_date: this.getRandomDate(new Date(2023, 0, 1), new Date()),
+        meds_date_complete: this.getRandomDate(new Date(2023, 0, 1), new Date()),
+        seizure_formdate: this.getRandomDate(new Date(2023, 0, 1), new Date()),
+        cb_date: this.getRandomDate(new Date(2023, 0, 1), new Date()),
+        vl_date: this.getRandomDate(new Date(2023, 0, 1), new Date()),
+        cbcl_survey_date: this.getRandomDate(new Date(2023, 0, 1), new Date()),
+        survey_quality_of_life_inventory_disability_timestamp: this.getRandomDate(new Date(2023, 0, 1), new Date()),
+        transition_page_completion_page_timestamp: this.getRandomDate(new Date(2023, 0, 1), new Date())
       });
     }
 
@@ -37,6 +46,28 @@ export class MockRedcapApiService {
   private getRandomAgeGroup(): string {
     const groups = ['0-6', '7-17', '18-65', '65+'];
     return groups[Math.floor(Math.random() * groups.length)];
+  }
+
+  private getRandomRace(): string {
+    const races = [
+      'American Indian or Alaksa Native',
+      'Asian',
+      'Native Hawaiian or Pacific Islander',
+      'Black or African American',
+      'White or Caucasian',
+      'Others',
+      'Unknown'
+    ];
+    return races[Math.floor(Math.random() * races.length)];
+  }
+
+  private getRandomEthnicity(): string {
+    const ethnicities = [
+      'Hispanic or Latino',
+      'Not Hispanic or Latino',
+      'Unknown'
+    ];
+    return ethnicities[Math.floor(Math.random() * ethnicities.length)];
   }
 
   private getRandomDate(start: Date, end: Date): string {
